eb8c493ad4fffc67251e031a71e0cbd5cd71ede5
salonichauhan1101 <salonichauhan1101@gmail.com>
2024-07-18 00:00:31 +0530

    a5 nodejs

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -1,4 +1,109 @@
+import AssignmentsControls from "./AssignmentsControls";
+import {BsGripVertical,BsPlus} from "react-icons/bs";
+import {BiSolidDownArrow} from "react-icons/bi";
+import LessonControlButtons from "./AssignmentsControlButtons";
 import {useParams} from "react-router";
+import {addAssignment, deleteAssignment, setAssignments, updateAssignment} from "./reducer";
+import {useSelector, useDispatch} from "react-redux";
+import * as client from "./client";
+import {useEffect} from "react";
+import { FaPlus } from "react-icons/fa";
+import { PiNotePencilFill } from "react-icons/pi";
+import { IoEllipsisVertical } from "react-icons/io5";
+
+export default function Assignments() {
+    const {cid} = useParams();
+
+    const {assignments} = useSelector((state: any) => state.assignmentsReducer);
+    const cidAssignments = assignments.filter((assignment: any) => assignment.course === cid);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        fetchAssignments();
+    }, []);
+
+    const fetchAssignments = async () => {
+        const assignments = await client.findAssignmentsForCourse(cid as string);
+        dispatch(setAssignments(assignments));
+    };
+    const removeAssignment = async (moduleId: string) => {
+        await client.deleteAssignment(moduleId);
+        dispatch(deleteAssignment(moduleId));
+    };
+    return (
+        <div id="wd-assignments">
+            <AssignmentsControls cid={cid!}/>
+
+<ul id="wd-assignment-list" className="list-group rounded-0 my-4">
+
+<div className="wd-title p-3 ps-2 bg-secondary">
+                      <div className="wd-title p-3 ps-1 bg-secondary bg-light w-100 d-flex align-items-center"
+                                                style={{justifyContent:"space-between"}}>
+                                                  <div >
+                                                <h4 id="wd-assignments-title">
+                                                <BsGripVertical className="me-2 fs-3" />
+                                                <BiSolidDownArrow className="me-2 fs-6" />
+                                                  <b>ASSIGNMENTS</b>
+                                                </h4> </div>
+                                                <div className = " d-flex align-items-center">
+                                          <div style={{
+                                            border: "1px solid #c7cdd158",
+                                            borderRadius: "25px",
+                                            padding: "5px 10px",
+                                            marginRight: "10px"
+                                          }}>
+                                         <button className="btn btn-outline-secondary me-2 d-flex align-items-center">
+                                         <b>40% of Total</b>
+                                         </button>
+                                          </div>
+                                          <BsPlus className="fs-4 me-2" />
+                                          <IoEllipsisVertical className="fs-4" />
+                                         </div>
+                                         </div>
+                                        </div>
+                {
+                    cidAssignments && cidAssignments.map((item: any) => (
+                        <li className="wd-assignment-list-item list-group-item p-3" style={{borderLeft: "4px solid green"}}>
+                            <div className="row align-items-center">
+                                <div className="col-auto">
+                                    <BsGripVertical className="fs-4"/>
+                                </div>
+                                <div className="col-auto">
+                                    <a className="wd-assignment-link text-dark link-underline link-underline-opacity-0"
+                                       href={`#/Kanbas/Courses/${cid}/Assignments/${item._id}`}>
+                                        <PiNotePencilFill className="text-success fs-4"/>
+                                    </a>
+
+                                </div>
+                                <div className="col">
+                                    <a className="wd-assignment-link text-dark link-underline link-underline-opacity-0"
+                                       href={`#/Kanbas/Courses/${cid}/Assignments/${item._id}`}>
+                                    <h5><b>{item.title}</b></h5>
+                                    </a>
+
+                                    <p>
+                                        <span className="text-danger"> Multiple Modules </span>
+                                        <b>| Not available until</b> {item.available} |<br /> <b>Due</b> {item.due} | {item.points} pts
+                                    </p>
+                                </div>
+                                <div className="col float-end">
+                                    <LessonControlButtons assignmentId={item._id}
+                                                          deleteAssignment={(assignmentId) => {
+                                                              dispatch(deleteAssignment(assignmentId));
+                                                          }}/>
+                                </div>
+                            </div>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+
+    );
+}
+
+
+/*import {useParams} from "react-router";
 import {deleteAssignment} from "./reducer";
 import {useSelector, useDispatch} from "react-redux";
 import {BsGripVertical,BsPlus} from "react-icons/bs";
@@ -87,5 +192,4 @@ export default function Assignments() {
         </div>
 
     );
-}
-
+}*/
--- src/Kanbas/Courses/Assignments/reducer.ts
+++ src/Kanbas/Courses/Assignments/reducer.ts
@@ -1,13 +1,15 @@
 import {createSlice} from "@reduxjs/toolkit";
-        import {assignments} from "../../Database";
-
         const initialState = {
-        assignments: assignments,
+        assignments: [],
         };
         const assignmentsSlice = createSlice({
         name: "assignments",
         initialState,
         reducers: {
+        setAssignments: (state, action) => {
+        state.assignments = action.payload;
+        },
+
         addAssignment: (state, {payload: assignment}) => {
         const newItem: any = {
         _id: new Date().getTime().toString(),
@@ -21,14 +23,17 @@ import {createSlice} from "@reduxjs/toolkit";
         };
         state.assignments = [...state.assignments, newItem] as any;
         },
+
         deleteAssignment: (state, {payload: assignmentId}) => {
         state.assignments = state.assignments.filter((m: any) => m._id !== assignmentId);
         },
+
         updateAssignment: (state, {payload: assignment}) => {
         state.assignments = state.assignments.map((m: any) =>
         m._id === assignment._id ? assignment : m
         ) as any;
         },
+
         editAssignment: (state, {payload: assignmentId}) => {
         state.assignments = state.assignments.map((m: any) =>
         m._id === assignmentId ? {...m, editing: true} : m
@@ -37,5 +42,6 @@ import {createSlice} from "@reduxjs/toolkit";
         },
         });
 
-        export const {addAssignment, deleteAssignment, updateAssignment, editAssignment} = assignmentsSlice.actions;
+        export const {addAssignment,deleteAssignment,updateAssignment,editAssignment,setAssignments}
+        = assignmentsSlice.actions;
         export default assignmentsSlice.reducer;
